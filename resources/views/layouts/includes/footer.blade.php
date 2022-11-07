fe994b053c5dfa8c5d442ff3b5cae3095e0988d8
rahman <rahman_023@smkassalaambandung.sch.id>
2022-11-07 11:22:19 +0700

    aku pusing ngoding sendiri

--- resources/views/layouts/includes/footer.blade.php
+++ resources/views/layouts/includes/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="main-footer d-flex justify-content-center" style="background-color:#000000" >
-    <strong>Copyright &copy; 2022 <a href="https://refalfalah.me">RefalFalah</a>.</strong>
+    <strong>Copyright &copy; 2022 <a href="https://instagram.com/n0tsmith?igshid=YmMyMTA2M2Y=">DidaAdityaSasmith</a>.</strong>
     All rights reserved.
 </footer>
